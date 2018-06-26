958217c3b64b475a09030eb010a1dcb0ecefde29
yarik7751 <yaroslav.levshunov2@gmail.com>
2018-06-27 00:22:54 +0300

    add interface

--- app/src/main/java/by/yarik/tagimage/activity/MainActivity.kt
+++ app/src/main/java/by/yarik/tagimage/activity/MainActivity.kt
@@ -1,13 +1,15 @@
-package by.yarik.tagimage
+package by.yarik.tagimage.activity
 
 import android.content.Intent
 import android.os.Bundle
-import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import by.yarik.tagimage.R
+import by.yarik.tagimage.adapter.ImageAdapter
 
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.content_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -16,22 +18,13 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
 
+        var imageAdapter = ImageAdapter(this);
+        gvImages.adapter = imageAdapter;
+
         fab.setOnClickListener { view ->
             /*Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                     .setAction("Action", null).show()*/
             startActivity(Intent(this, AddImageActivity::class.java));
-        }
-    }
-
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        menuInflater.inflate(R.menu.menu_main, menu)
-        return true
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        return when (item.itemId) {
-            R.id.action_settings -> true
-            else -> super.onOptionsItemSelected(item)
         }
     }
 }
